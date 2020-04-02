a17bf15e308f1b647eba8c75a994782e3de10917
Tiago Ferreira <tiago@weekend.com>
2020-04-02 18:21:08 +0100

    move js to separated file

---
+++ website-hugo/src/themes/covid19/assets/js/checkcode.js
@@ -0,0 +1,19 @@
+function trackEvent(action,name,value) {
+    if (_paq && _paq.push) {
+        _paq.push(['trackEvent', 'checkcode', action, name, value]);
+    }
+}
+
+function checkCode(){
+    let token = document.getElementById('token').value;
+    if (token.length == 0) {
+        document.getElementById('checking').innerHTML = '{{ i18n "fail_code_not_valid" }}'
+        trackEvent('click','valid', false);
+        return;
+    }
+    trackEvent('click','valid', true);
+    document.getElementById('checking').innerHTML = '{{ i18n "checking" }}'
+    setTimeout(function() {
+        document.getElementById('checking').innerHTML = '{{ i18n "no_match" }}'
+    }, 1000);
+}
