09b0b8ca900df7c2c308fc179540b553078ac4c9
Tiago Ferreira <tiago@weekend.com>
2020-04-06 16:48:55 +0100

    integrate manual data with upload

--- website-hugo/src/themes/covid19/assets/js/covid.js
+++ website-hugo/src/themes/covid19/assets/js/covid.js
@@ -4,6 +4,55 @@ var startDate = new Date();
 var endDate = new Date();
 var formState = {};
 
+var mymap = L.map('mapid').setView([38.7436214, -9.1953085], 13);
+var markers = {};
+var accessToken = "pk.eyJ1IjoidGlhZ29yYmYiLCJhIjoiY2s4bmUybTRoMDg1bDNsbHZxNjZtZWVubCJ9.2E1KHgavQ9HeTV_aoTxGRw";
+    
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+var searchControl = new L.esri.Controls.Geosearch().addTo(mymap);
+var results = new L.LayerGroup().addTo(mymap);
+
+searchControl.on('results', function(data){
+    results.clearLayers();
+    for (var i = data.results.length - 1; i >= 0; i--) {
+        mymap.setView(data.results[i].latlng, 8);
+    }
+});
+
+mymap.on('click', onMapClick);
+
+function onMapClick(e) {
+    var mp = new L.Marker([e.latlng.lat, e.latlng.lng]).addTo(mymap);
+    mp.bindPopup('<a style="cursor:pointer" onclick=\'removeMarker("' + mp._leaflet_id + '")\'>Remove</a>');
+    markers[mp._leaflet_id] = mp;
+}
+
+function removeMarker(e){
+    mymap.removeLayer(markers[e]);
+    delete markers[parseInt(e)];
+}
+
+function getMapLocations(){
+    var locations = [];
+    for (var key of Object.keys(markers)) {
+        coordinates = {
+            "timestamp": Date.now(),
+            "lat": markers[key].getLatLng().lat,
+            "long": markers[key].getLatLng().lng
+        }
+        locations.push(coordinates)
+    }
+    return locations;
+}
+
 function trackEvent(action,name,value) {
     if (_paq && _paq.push) {
         _paq.push(['trackEvent', 'upload', action, name, value]);
@@ -104,13 +153,17 @@ function handleFileSelect(evt) {
 }
 
 function handleUpload() {
-    if (filteredLocations.length) {
+    var locations = filteredLocations;
+    if (locations.length == 0) {
+        locations = getMapLocations();
+    }
+    if (locations.length) {
         setElementVisibility('upload-btn', false)
         showArea("response");
         showInformation('uploadstatus','{{ i18n "uploading" }}');
 
         sendData({
-            "locations": filteredLocations,
+            "locations": locations,
             "tested" : formState["tested"],
             "testedDate" : formState["testedDate"].getTime() || undefined,
             "positive" : formState["positive"],
